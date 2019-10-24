8550fcbbf7ba42e62b7364fa74f904c66090916f
Pougliche <pugliese.viktor@gmail.com>
2019-10-24 23:36:39 +0200

    modif seed & gemfile

--- Gemfile
+++ Gemfile
@@ -54,5 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'faker'
-
+gem 'colorize'
 gem 'table_print'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
 require 'faker'
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'colorize'
 
 20.times do |index|
     city = City.create!(city_name: Faker::Address.city)
@@ -14,9 +7,9 @@ end
 
 100.times do |index|
 dog_sitter = DogSitter.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: City.all.sample)
-
-dog = Dog.create!(dog_name: Faker::Creature::Dog.name, breed_name: Faker::Creature::Dog.breed, gender: Faker::Creature::Dog.gender , city: City.all.sample)
-
-stroll = Stroll.create(dog_sitter: dog_sitter, dog: Dog.all.sample, city: dog_sitter.city, stroll_time: Faker::Time.forward(days: 30))
+    dog = Dog.create!(dog_name: Faker::Creature::Dog.name, breed_name: Faker::Creature::Dog.breed, gender: Faker::Creature::Dog.gender , city: City.all.sample)
+        stroll = Stroll.create(dog_sitter: dog_sitter, dog: Dog.all.sample, city: dog_sitter.city, stroll_time: Faker::Time.forward(days: 30))
 
 end
+
+puts "20 CITIES HAVE BEEN CREATED. 100 DOGGOS, DOGSITTERS AND STROLLS HAVE BEEN CREATED. WIF WIF!".yellow
